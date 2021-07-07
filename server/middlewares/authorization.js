3044580fb6a5c3a09c28c8bce42b0ef16c325840
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-07 19:47:54 +0700

    commit 2

---
+++ server/middlewares/authorization.js
@@ -0,0 +1,15 @@
+const Wishlist = require("../models");
+
+const authorization = (req, res, next) => {
+  Wishlist.findAll({
+    where: {
+      user_id: req.userId,
+    },
+  })
+    .then((result) => {
+      req.authorizedWishlists = result;
+    })
+    .catch((err) => next(err));
+};
+
+module.exports = authorization;
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,5 +1,9 @@
 const routers = require("express").Router();
-const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { User, Wishlist } = require("../models");
+const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
 
 routers.post("/register", (req, res) => {
   const { email, password } = req.body;
@@ -13,9 +17,49 @@ routers.post("/register", (req, res) => {
     })
     .catch((err) => console.log(err));
 });
-routers.post("/login", (req, res) => {});
-routers.get("/wishlists", (req, res) => {});
-routers.post("/wishlists", (req, res) => {});
-routers.delete("/wishlists/:id", (req, res) => {});
+routers.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({
+    where: {
+      email: email,
+    },
+  })
+    .then((result) => {
+      if (result && bcrypt.compareSync(password, result.dataValues.password)) {
+        const payload = {
+          id: result.dataValues.id,
+        };
+        const access_token = jwt.sign(payload, process.env.JWT_SECRET);
+        res.status(200).json({ access_token: access_token });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+routers.get("/wishlists", [authentication, authorization], (req, res) => {});
+routers.post("/wishlists", authentication, (req, res) => {
+  const { name, image_url, price, description } = req.body;
+  Wishlist.create({
+    user_id: req.userId,
+    name: name,
+    image_url: image_url,
+    price: price,
+    description: description,
+  })
+    .then((result) => {
+      // res.status(201).json({
+      //     name: result.dataValues.name,
+      //     price: result.dataValues.price,
+      //     image_url: result.dataValues.image_url,
+      //     userId: result.dataValues.
+      // })
+      console.log(result);
+    })
+    .catch((err) => console.log(err));
+});
+routers.delete(
+  "/wishlists/:id",
+  [authentication, authorization],
+  (req, res) => {}
+);
 
 module.exports = routers;
