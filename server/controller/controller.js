393a695f700cb8680642c881724dfa4c126742e5
ardiansyakh <abdullah.ard9@gmail.com>
2021-07-07 20:44:55 +0700

    commit4

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -22,9 +22,9 @@ class UserController{
         .then(result=>{
             if(result===0) throw({err: 'User Not Found'})
             const hash = result.password
-            bcrypt.compare(password, hash, function(err, res) {
-                if(res){
-                    const access_token = jwt.sign({ id: result.id }, process.env.JWT_SECRET);
+            bcrypt.compare(password, hash, function(err, result2) {
+                if(result2){
+                    const access_token = jwt.sign({ id: result.id, saldo: result.saldo }, process.env.JWT_SECRET);
                     res.status(200).json({access_token})
                 }else{
                     throw({err: 'INVALID PASSWORD'})
@@ -40,8 +40,10 @@ class UserController{
 class WishlistController{
     static getWishlists(req, res, next){
         const {access_token} = req.headers
+        const decoded = jwt.verify(access_token, process.env.JWT_SECRET);
+        const UserId = decoded.id
         Wishlist.findAll({
-            where: {UserId: id}
+            where: {UserId}
         })
         .then(result=>{
             res.status(200).json({result})
@@ -52,11 +54,65 @@ class WishlistController{
     }
 
     static addWishlists(req, res, next){
+        const {access_token} = req.headers
+        const decoded = jwt.verify(access_token, process.env.JWT_SECRET);
+        const {UserId, saldo} = decoded
+        const {name, image_url, price, description} = req.body
+        let wishlistAdded
+        if(saldo<price){
+            throw({err: 'SALDO TIDAK CUKUP'})
+        }
+        Wishlist.create({name, image_url, price, description, UserId})
 
+        .then(result=>{
+            wishlistAdded = result
+            const newSaldo = saldo - price
+            return User.update({saldo: newSaldo}, {
+                where:{
+                    id: UserId
+                }
+            })    
+        })
+        .then(result=>{
+            saldo = result.saldo
+            res.status(200).json({wishlistAdded, saldo})
+        })
+
+        .catch(err=>{
+            status(400).json({err})
+        })
     }
 
     static deleteWishlists(req, res, next){
-
+        const wishlistId = req.params
+        const {access_token} = req.headers
+        const decoded = jwt.verify(access_token, process.env.JWT_SECRET);
+        const {UserId, saldo} = decoded
+        let price
+        Wishlist.findOne({
+            where: {id: wishlistId}
+        })
+        .then(result=>{
+            price = result.price
+            return Wishlist.destroy({
+                where:{id: wishlistId}
+            })
+        })
+        .then(_=>{
+            const newSaldo = saldo + price
+            return User.update({saldo: newSaldo}, {
+                where:{
+                    id: UserId
+                }
+            })  
+        })
+        .then(result=>{
+            const saldo = result.saldo
+            res.status(200).json({"message": "Successfully delete Wishlist", saldo})
+        })
+        .catch(err=>{
+            
+        })
     }
 }
 
