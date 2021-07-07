19733a7f3827f37c4f34c00835ae895d6c2b9961
UmamKhoerul <umam.itdev@gmail.com>
2021-07-07 19:42:42 +0700

    2

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 const {User} = require('../models')
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 class userControllers{
     static createUser(req,res,next){
         const {email,password} = req.body
@@ -13,9 +14,15 @@ class userControllers{
     }
     static login(req,res,next){
         const {email,password} = req.body
-        User.findOne({email,password})
+        User.findOne({where:{email:email}})
         .then(data=>{
-            res.status(201).json({email:data.email,password:data.password})
+            const compassword =  bcrypt.compareSync(password, data.password); 
+            const jwt = process.env.JWT_KEY
+            if (data && compassword) {
+                const paylod = {id:data.id}
+                var access_token = jwt.sign(paylod, jwt);
+                res.status(200).json({email:data.email,password:data.password})
+            }
         })
         .catch(err=>{
             next(err)
---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+const router = require('./routes')
+const app = express()
+const PORT = 3000
+
+app.use(express.urlencoded({extended:true}))
+app.use(router)
+
+app.listen(PORT,()=>{
+    console.log('listen to ' + PORT);
+})
